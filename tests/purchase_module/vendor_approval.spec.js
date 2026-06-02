f35920b0288e161111709016dfd507ddb52eff87
MD SOHAIL AHMAD <50292@manappuram.com>
2026-06-02 14:36:06 +0530

    vendor approval success

--- tests/purchase_module/vendor_approval.spec.js
+++ tests/purchase_module/vendor_approval.spec.js
@@ -1,11 +1,11 @@
-import { test } from '@playwright/test';
+import { test,expect } from '@playwright/test';
 import { type } from 'node:os';
 
 const INDEX_URL = 'https://uatapp.manappuram.net/purchase/Index/Index.aspx';
 
 test.describe.serial('Vendor Management approval Flow', () => {
 
-  test('Test-1: Site approval – vendor first, then site', async ({ page }) => {
+  test.skip('Test-1: Site approval – vendor first, then site', async ({ page }) => {
 
     await page.goto(INDEX_URL);
 
@@ -26,7 +26,7 @@ test.describe.serial('Vendor Management approval Flow', () => {
 
     /* ✅ STEP 2: SELECT VENDOR (PARTIAL MATCH) */
     await page.evaluate(() => {
-      const vendorPartialName = 'TEST_1778153977640'; // use PARTIAL text
+      const vendorPartialName = 'TEST_1780053177512'; // use PARTIAL text
       const ddl = document.querySelector('#ddlVendor1');
 
       const match = Array.from(ddl.options).find(o =>
@@ -82,19 +82,66 @@ test.describe.serial('Vendor Management approval Flow', () => {
 
     // Open Site tab
     await page.getByRole('tab', { name: 'Bank Details' }).click();
-    await page.getByPlaceholder('Vendor ....').fill('TEST_1778153977640');
-    await page.locator('input[onclick="SrchVendorNeftApp()"]').click({ force: true });
+    await page.getByPlaceholder('Vendor ....').type('TEST');
+    await page.locator('#ddlVendor2autocomplete-list').waitFor({ state: 'visible', timeout: 30000 });
+    await page.locator('#ddlVendor2autocomplete-list').getByText('TEST_1777698444815').click();
+    await page.waitForTimeout(1000);
+    await page.getByRole('button', { name: 'Search' }).click({ force: true });
 
     await page.waitForFunction(() => {
       const d = document.querySelector('#ddlSites');
       return d && d.options.length > 1;
     });
 
-    await page.selectOption('#ddlSite', { label: 'CHENNAI SITE' });
+    await page.selectOption('#ddlSites', { label: 'CHENNAI SITE' });
     await page.waitForTimeout(1000);
     await page.locator('#txtRejectRemark2').fill('test roll')
     await page.getByRole('button', { name: 'Add NEFT' }).click();
     //await page.getByRole('button', { name: 'OK' }).click();
+
+});
+
+test('Test-3: Documents approval', async ({ page }) => {
+
+    await page.goto(INDEX_URL);
+
+    // Navigate
+    await page.getByRole('link', { name: 'VENDOR MANAGEMENT' }).click();
+    await page
+      .locator('//a[@href="../Purchase/VendorManagmnt.aspx?frmid=2"]')
+      .click();
+
+    // Open Site tab
+    await page.getByRole('tab', { name: 'Documents' }).click();
+
+await page.selectOption('#ddlVendorDoc', {
+  label: 'ABE ADVERTISING'
+});
+await page.waitForTimeout(2000);
+
+// Loop until no "Verify" buttons left
+while (await page.locator('#tblVendorDoc button:has-text("Verify")').count() > 0) {
+
+  const row = page.locator('#tblVendorDoc tr')
+    .filter({ has: page.locator('button:has-text("Verify")') })
+    .first();
+
+  // Click View
+  await row.locator('button:has-text("View")').click();
+  await page.waitForLoadState('networkidle');
+
+  // Click Verify
+  await row.locator('button:has-text("Verify")').click();
+  await page.waitForLoadState('networkidle');
+ }
+
+ await expect(page.getByRole('button', { name: 'Exit' })).toBeVisible({ timeout: 30000 });
+
+  await page.getByRole('button', { name: 'Exit' }).click();
+
+     
+
+   // await page.getByRole('button', { name: 'Exit' }).click();
 
 });
 });
